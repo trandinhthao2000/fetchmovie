0e0c5576b4aed5dca79b7fee3405679835e52226
trandinhthao2000 <71816109+trandinhthao2000@users.noreply.github.com>
2022-02-17 14:49:02 +0700

    Updates

---
+++ precache-manifest.c84cc34da76c60dddfe67b5005fc965c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6a5c2057d3389917f838e890dba916",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3eff9eba12acd53a6084",
+    "url": "./static/css/main.e735219a.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "./static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eff9eba12acd53a6084",
+    "url": "./static/js/main.0e046f0d.chunk.js"
+  },
+  {
+    "revision": "0df309de63531330f53a",
+    "url": "./static/js/runtime-main.5369ff77.js"
+  }
+]);
